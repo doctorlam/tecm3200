d563c9a842e27a95ecbd20cd91c4f092a7b48755
Chris <chris.lam.unt@gmail.com>
2017-01-05 17:20:43 -0600

    removed topic filter for now

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -8,10 +8,10 @@ class LessonsController < ApplicationController
   def index
   @search = Lesson.search(params[:q])
   @search.sorts = 'date' if @search.sorts.empty?
-
   @results = @search.result
   @results = @results.where(:lesson_type => "Main") unless params[:q]
   @lessons = @results
+  
 end
 
 
