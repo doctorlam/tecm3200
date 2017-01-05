82a4eff1ab63eea191262a9235a117dc26f35f12
Chris <chris.lam.unt@gmail.com>
2017-01-05 13:03:39 -0600

    remove week filter

--- app/helpers/lessons_helper.rb
+++ app/helpers/lessons_helper.rb
@@ -3,6 +3,6 @@ module LessonsHelper
 		['Main', 'Lab']
 	end
 	def options_for_week
-		['1', '2', '3', '4', '5','6', '7', '8','9', '10','11','12','13','14','15']
+		['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5','Week 6', 'Week 7', 'Week 8','Week 9', 'Week 10','Week 11','Week 12','Week 13','Week 14','Week 15']
 	end
 end
