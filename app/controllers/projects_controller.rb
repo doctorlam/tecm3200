ec00892412fac6a1e7abed6ccff0144deae7824a
Chris <chris.lam.unt@gmail.com>
2017-01-06 16:59:10 -0600

    fixed ordering

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -6,7 +6,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.order(due: :asc)
+    @projects = Project.all.reorder(due: :asc)
 
   end
 
