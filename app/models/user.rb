364ecaf16b9f381eb8efedd65c59d67d32679ebf
Chris <chris.lam.unt@gmail.com>
2018-01-04 15:50:08 -0600

    removed any dependenices on users

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ActiveRecord::Base
   has_many :lessons
    has_many :projects 
    has_many :abouts
-   has_many :submissions, dependent: :destroy
+   has_many :submissions
    has_attached_file :avatar, :styles => { :medium => "150x150>", :thumb => "30x30" }, :default_url => "missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   acts_as_commontator
-  has_many :posts, dependent: :destroy
-  has_many :responses, dependent: :destroy
+  has_many :posts
+  has_many :responses
   has_many :presentations
 
 
