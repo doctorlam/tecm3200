edf8aff9b7beeac39145932d17f4ee9aa57fc876
Chris <chris.lam.unt@gmail.com>
2018-01-04 15:42:52 -0600

    changed dependencies for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   validates :name, presence: true
-  has_many :lessons, dependent: :destroy 
-   has_many :projects, dependent: :destroy 
-   has_many :abouts, dependent: :destroy
+  has_many :lessons
+   has_many :projects 
+   has_many :abouts
    has_many :submissions, dependent: :destroy
    has_attached_file :avatar, :styles => { :medium => "150x150>", :thumb => "30x30" }, :default_url => "missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   acts_as_commontator
   has_many :posts, dependent: :destroy
   has_many :responses, dependent: :destroy
-  has_many :presentations, dependent: :destroy
+  has_many :presentations
 
 
 end
